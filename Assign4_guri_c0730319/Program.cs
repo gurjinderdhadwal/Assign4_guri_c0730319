6ae358b08848232a501d4b6a7ef67432d8e9d38a
visualstudio <visualstudio@122-PC23>
2019-04-05 21:52:43 -0400

    sectionc

--- Assign4_guri_c0730319/Program.cs
+++ Assign4_guri_c0730319/Program.cs
@@ -4,94 +4,210 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Assign4_guri_c0730319
 {
     class Program
+
     {
+
         ArrayList Beowulf;
 
 
+
         static void Main(string[] args)
+
         {
+
             Program p = new Program();
 
-            p.ReadTextFiles();
             p.Beowulf = new ArrayList();
 
+            p.Wordfinder();
+
             Console.ReadLine();
-        }
-        public void Run()
-        {
-            this.ReadTextFiles();
+
+
+
         }
 
-        public void ReadTextFiles()
+        public void Run()
+
         {
+
+            this.ReadTextFiles();
+
+        }
+
+
+
+        public void ReadTextFiles()
+
+        {
+
+
+
             // Read file using StreamReader. Read file line by line
+
             using (StreamReader file = new StreamReader("U:/Users/730319/Assign4_guri_c0730319/Assign4_guri_c0730319/Beowulf.txt"))
 
             {
 
                 int counter = 0;
+
                 string ln;
 
+
+
                 while ((ln = file.ReadLine()) != null)
+
                 {
+
                     Console.WriteLine(ln);
+
+                    Beowulf.Add(ln);
+
                 }
+
                 file.Close();
-                counter = File.ReadLines("U:/Users/730319/Assign4_guri_c0730319/Assign4_guri_c0730319/Beowulf.txt").Count();
-                Console.WriteLine("\n\n\nThe file has " + counter + " lines");
+
+                Console.WriteLine($"File has {counter} lines.");
+
             }
+
         }
+
         public int FindNumberOfBlankSpaces(string line)
+
         {
 
             int countletters = 0;
+
             int countSpaces = 0;
 
-            foreach (char c in line)
             {
-                if (char.IsLetter(c)) { countletters++; }
 
-                if (char.IsWhiteSpace(c)) { countletters++; }
+                foreach (char c in line)
+
+                {
+
+                    if (char.IsLetter(c)) { countletters++; }
+
+                    if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+                }
+
+                return countSpaces;
+
             }
-            return countSpaces;
 
         }
 
-        public long TotalWords()
+        public void CountLinesReader()
+
         {
 
-            StreamReader reader = new StreamReader("Beowulf.txt");
+            long lineCounter = 0;
+
+            using (StreamReader fil = new StreamReader("U:/Users/730319/Assign4_guri_c0730319/Assign4_guri_c0730319/Beowulf.txt"))
+
+            {
+
+                while (fil.ReadLine() != null)
+
+                {
+
+                    lineCounter++;
+
+                }
+
+                Console.WriteLine(lineCounter);
+
+            }
+
+        }
+
+        public void WordCounter()
+
+        {
+
+
+
+            StreamReader reader = new StreamReader("U:/Users/730319/Assign4_guri_c0730319/Assign4_guri_c0730319/Beowulf.txt");
+
             string script = reader.ReadToEnd();
 
+
+
             var text = script.Trim();
-            long wordCount = 0;
-            int index = 0;
+
+            int wordCount = 0, index = 0;
+
+
 
             while (index < text.Length)
+
             {
+
                 // check if current char is part of a word
+
                 while (index < text.Length && !char.IsWhiteSpace(text[index]))
+
                     index++;
+
+
 
                 wordCount++;
 
+
+
                 // skip whitespace until next word
+
                 while (index < text.Length && char.IsWhiteSpace(text[index]))
+
                     index++;
+
             }
 
-            Console.WriteLine("The file has " + wordCount);
-            return wordCount;
+
+
+            Console.WriteLine(wordCount);
+
+
+
+
+
+        }
+
+        public void Wordfinder()
+
+        {
+
+            int f = 0;
+
+            foreach (var line in File.ReadAllLines("U:/Users/730319/Assign4_guri_c0730319/Assign4_guri_c0730319/Beowulf.txt"))
+
+            {
+
+                if (line.Contains("sea") && line.Contains("fare"))
+
+                {
+
+                    f++;
+
+                }
+
+
+
+            }
+
+            Console.WriteLine(f);
 
         }
 
     }
+
+
+
 }
-    
-   
